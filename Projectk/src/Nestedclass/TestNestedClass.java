97124897db0ac6c5a2fd38e8572c03e425d64c5d
fahim909 <42551138+fahim909@users.noreply.github.com>
2018-08-31 14:40:47 -0400

    Update TestNestedClass.java

--- Projectk/src/Nestedclass/TestNestedClass.java
+++ Projectk/src/Nestedclass/TestNestedClass.java
@@ -7,6 +7,6 @@ public class TestNestedClass {
     public static void main(String[] args) {
         OuterClass outerclass = new OuterClass();
         outerclass.outerAMethodA();
-        outerclass.innerA.innerAMethodA()
+        outerclass.innerA.innerAMethodA();
     }
 }
